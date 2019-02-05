3c4e48f39e5f1206b16f2108e14976f07719d9b7
Aashish Sharma <aasyspoudyal@hotmail.com>
2019-02-05 07:54:14 -0600

    adding functionality to side drawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -4,8 +4,15 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import MenuIcon from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
+import SideDrawer from './SideDrawer'
 
 class Header extends Component {
+  state = {
+    drawerOpen: false
+  }
+  toggleDrawer = value => {
+    this.setState({ drawerOpen: value })
+  }
   render () {
     return (
       <AppBar
@@ -29,6 +36,10 @@ class Header extends Component {
           >
             <MenuIcon />
           </IconButton>
+          <SideDrawer>
+            open={this.state.drawerOpen}
+            onClose={value => this.toggleDrawer(value)}
+          </SideDrawer>
         </Toolbar>
       </AppBar>
     )
